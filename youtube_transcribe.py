789fdf98c20fa2ae58f7f7a5c6e97f3ce0e84303
JHIH-SIOU LI <jsli@linux.com>
2026-02-05 23:47:13 +0800

    chore: try

--- youtube_transcribe.py
+++ youtube_transcribe.py
@@ -64,7 +64,7 @@ class YouTubeTranscriber:
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
             },
-            'cookiesfrombrowser': ('firefox', 'default', None, 'Meta')
+            'cookiesfrombrowser': ('firefox',),
         }
         
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
