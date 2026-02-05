3ecd06bbc4e9d4a791951501bc50f3bb4c98d31d
JHIH-SIOU LI <jsli@linux.com>
2026-02-05 23:20:49 +0800

    chore: try to be nice

--- youtube_transcribe.py
+++ youtube_transcribe.py
@@ -63,7 +63,8 @@ class YouTubeTranscriber:
             'extractor_args': {'youtube': {'skip': ['dash', 'hls']}},  # 跳过有问题的格式
             'http_headers': {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-            }
+            },
+            'cookiesfrombrowser': ('firefox')
         }
         
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
